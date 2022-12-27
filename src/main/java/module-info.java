6683ad327cc3df71d89537d5a7ce4118f9914b01
Kah Yeek <kycheok2007@gmail.com>
2022-12-27 17:27:49 +0800

    config: updated module info for exporting new packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,12 @@ module com.blastoisefx {
     requires javafx.fxml;
 
     opens com.blastoisefx to javafx.fxml;
+    opens com.blastoisefx.controller to javafx.fxml;
+    opens com.blastoisefx.model to javafx.fxml;
+    opens com.blastoisefx.utils;
     exports com.blastoisefx;
+    exports com.blastoisefx.controller;
+    exports com.blastoisefx.model;
+    exports com.blastoisefx.utils;
+
 }
