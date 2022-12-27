59191541947db343dc8c0d0ca4c3d4b22bbfc0f9
Kah Yeek <kycheok2007@gmail.com>
2022-12-27 17:30:36 +0800

    feat: add auth class for handling login and sign up

---
+++ src/main/java/com/blastoisefx/model/Auth.java
@@ -0,0 +1,31 @@
+package com.blastoisefx.model;
+
+import com.blastoisefx.App;
+import com.blastoisefx.utils.Rgx;
+
+public class Auth {
+    public static boolean validate(String email, String password) {
+        for (User user : App.getUsers()) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password))
+                return true;
+        }
+        return false;
+    }
+
+    public static int addNewUser(String email, String password) {
+        if (Rgx.regexValidateEmail(email) && password.length() >= 8) {
+            App.getUsers().add(new User(email, password));
+            return 0;
+        }
+        else if(!Rgx.regexValidateEmail(email) && password.length() >= 8){
+            return 1;
+        }
+        else if(Rgx.regexValidateEmail(email) && !(password.length() >= 8)){
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+
+}
