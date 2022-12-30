d8eeef31c8ac9056e8b12f8430abb1b44ec4cee0
Kah Yeek <kycheok2007@gmail.com>
2022-12-30 17:26:58 +0800

    feat: update model file for machinetypes

--- src/main/java/com/blastoisefx/model/MachineTypes.java
+++ src/main/java/com/blastoisefx/model/MachineTypes.java
@@ -1,5 +1,38 @@
 package com.blastoisefx.model;
 
 public class MachineTypes {
+    private int duration;
+    private double price;
+    public enum MachineState{
+        IDLE,
+        WASHING,
+        LOCKED,
+    }
 
+    private MachineState machineState;
+
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int processTime) {
+        this.duration = processTime;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public MachineState getMachineState(){
+        return machineState;
+    }
+    public void setState(MachineState machineState){
+        this.machineState = machineState;
+    }
+
+    // Constuctor here
+    public MachineTypes(){
+
+    }
 }
