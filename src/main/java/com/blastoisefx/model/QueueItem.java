cbc733856da97e6e94f4ec69c696fb338d70ff3e
Kah Yeek <kycheok2007@gmail.com>
2022-12-30 17:26:37 +0800

    feat: improve model file for queueItem to be added in queue

--- src/main/java/com/blastoisefx/model/QueueItem.java
+++ src/main/java/com/blastoisefx/model/QueueItem.java
@@ -1,14 +1,72 @@
 package com.blastoisefx.model;
 
 import java.time.LocalDateTime;
-import java.time.Period;
 
 public class QueueItem {
+    private User user;
     private LocalDateTime startTime;
-    private Period duration;
+    private MachineTypes machineTypes;
 
-    public QueueItem(){
+    private int duration;
 
+    public enum State {
+        WAITING,
+        WASHING,
+        FINISHED
+    }
+
+    private State state;
+
+    // GETTERS AND SETTERS FOR EVERYTHING STARTS HERE
+    public MachineTypes getMachineTypes() {
+        return machineTypes;
+    }
+
+    public void setMachineTypes(MachineTypes machineTypes) {
+        this.machineTypes = machineTypes;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    // ENDS HERE
+
+    // CONSTRUCTOR
+    public QueueItem(User user, LocalDateTime startTime, MachineTypes machineTypes) {
+        this.user = user;
+        this.startTime = startTime;
+        this.machineTypes = machineTypes;
+        this.state = State.WAITING;
+        this.duration = machineTypes.getDuration();
     }
 
 }
