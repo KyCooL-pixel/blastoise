a7f4937f14282aacdf2101d9dbd42e0b696d296c
Chuo <ngiubing02@gmail.com>
2023-02-10 11:24:31 +0800

    feat: add dialog for duration and payment method

--- src/main/java/com/blastoisefx/controller/ClientController.java
+++ src/main/java/com/blastoisefx/controller/ClientController.java
@@ -2,11 +2,14 @@ package com.blastoisefx.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import com.blastoisefx.App;
 import com.blastoisefx.model.Payment;
 import com.blastoisefx.model.User;
+import com.blastoisefx.model.Washer;
+import com.blastoisefx.model.Payment.Method;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +17,9 @@ import javafx.fxml.Initializable;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -27,7 +32,6 @@ public class ClientController implements Initializable {
 
   @FXML
   private Label emailLabel;
-
 
   @FXML
   private Label title;
@@ -43,7 +47,6 @@ public class ClientController implements Initializable {
     this.user = user;
   }
 
-
   public void setUser(User user) {
     this.user = user;
   }
@@ -58,18 +61,42 @@ public class ClientController implements Initializable {
     FXMLLoader loader = App.getFXMLLoader("payment");
     Scene paymentScene = new Scene(loader.load(), 335, 600);
     stage.setScene(paymentScene);
+
+    var duration = getDuration();
+    var price = duration * 123123;
+
     queueController.addWasherQueue(
-      user,
-      new Payment(50, Payment.Method.ONLINE_BANKING),
-      50
-    );
+        user,
+        new Payment(price, getPaymentMethod()),
+        50);
   }
 
-  public void setStage(Stage thisStage){
+  public void setStage(Stage thisStage) {
     stage = thisStage;
+  }
+
+  private Method getPaymentMethod() {
+    ChoiceDialog<Method> dialog = new ChoiceDialog<Method>(Method.ONLINE_BANKING, Method.values());
+
+    var result = dialog.showAndWait();
+    while (!result.isPresent()) {
+      result = dialog.showAndWait();
+    }
+    return result.get();
+  }
+
+  private double getDuration() {
+    TextInputDialog dialog = new TextInputDialog("0.0");
+    dialog.setContentText("Please enter the duration of your laundry (in seconds):");
+    var result = dialog.showAndWait();
+    try {
+      while (!result.isPresent() || Double.parseDouble(result.get()) < 0) {
+        result = dialog.showAndWait();
+      }
+    } catch (NumberFormatException e) {
+      result = dialog.showAndWait();
+    }
+    return Double.parseDouble(result.get());
+  }
+
 }
-
-
-
-}
-
