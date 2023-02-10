05da9bf5538773a7c34a40503c6256a03262f943
Chuo <ngiubing02@gmail.com>
2023-02-10 20:57:37 +0800

    feat: add view machines screen

---
+++ src/main/java/com/blastoisefx/controller/ClientViewMachinesController.java
@@ -0,0 +1,153 @@
+package com.blastoisefx.controller;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import com.blastoisefx.App;
+import com.blastoisefx.model.Machine;
+import com.blastoisefx.model.QueueItem;
+import com.blastoisefx.model.User;
+
+import javafx.collections.ListChangeListener;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ClientViewMachinesController implements Initializable {
+
+  @FXML
+  private Button backButton;
+
+  @FXML
+  private Button lockButton;
+
+  @FXML
+  private ListView<QueueItem> queuesListView;
+
+  private Scene clientMenuScene;
+  private User user;
+  private HashMap<QueueItem, Machine> queueItemToMachine = new HashMap<>();
+
+  public ClientViewMachinesController(Scene clientMenuScene, User user) {
+    this.clientMenuScene = clientMenuScene;
+    this.user = user;
+    var queue = user.getQueue();
+
+    queue.forEach(item -> {
+      var machine = getMachineFromQueueItem(item);
+      if (machine != null) {
+        queueItemToMachine.put(item, machine);
+      }
+    });
+    queue.addListener(new ListChangeListener<QueueItem>() {
+      @Override
+      public void onChanged(Change<? extends QueueItem> c) {
+        while (c.next()) {
+
+          for (var item : c.getAddedSubList()) {
+            var machine = getMachineFromQueueItem(item);
+            if (machine != null) {
+              queueItemToMachine.put(item, machine);
+            }
+          }
+          for (var item : c.getRemoved()) {
+            queueItemToMachine.remove(item);
+          }
+        }
+      }
+    });
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    backButton.onActionProperty().set(e -> {
+      Stage stage = (Stage) backButton.getScene().getWindow();
+      stage.setScene(clientMenuScene);
+    });
+
+    lockButton.setOnAction(e -> {
+      var item = queuesListView.selectionModelProperty().get().getSelectedItem();
+      var machine = queueItemToMachine.get(item);
+      if (machine.getStatus() == Machine.Status.WAITING) {
+        machine.setStatus(Machine.Status.LOCKED);
+      } else if (machine.getStatus() == Machine.Status.LOCKED) {
+        machine.setStatus(Machine.Status.WAITING);
+      }
+    });
+
+    queuesListView.onMouseClickedProperty().set(e -> {
+      var item = queuesListView.selectionModelProperty().get().getSelectedItem();
+
+      if (item == null) {
+        lockButton.setDisable(true);
+        return;
+      }
+
+      var machine = queueItemToMachine.get(item);
+
+      if (machine == null) {
+        lockButton.setDisable(true);
+        return;
+      }
+
+      lockButton.setDisable(false);
+      if (machine.getStatus() == Machine.Status.WAITING) {
+        lockButton.setText("Unlock");
+      } else if (machine.getStatus() == Machine.Status.LOCKED) {
+        lockButton.setText("Lock");
+      } else {
+        lockButton.setDisable(true);
+      }
+    });
+    queuesListView.setItems(user.getQueue());
+    queuesListView.setCellFactory(param -> new ListCell<QueueItem>() {
+      private final Label userEmailLabel = new Label();
+      private final Label endTimeLabel = new Label();
+      private final AnchorPane layout = new AnchorPane(userEmailLabel, endTimeLabel);
+
+      {
+        AnchorPane.setLeftAnchor(userEmailLabel, 2.0);
+        AnchorPane.setRightAnchor(endTimeLabel, 2.0);
+      }
+
+      @Override
+      protected void updateItem(QueueItem item, boolean empty) {
+        super.updateItem(item, empty);
+        if (empty || item == null || item.getUser() == null || item.getOperationEndTime() == null) {
+          userEmailLabel.setText(null);
+          endTimeLabel.setText(null);
+          setGraphic(null);
+        } else {
+          userEmailLabel.setText("halo");
+          endTimeLabel.setText(item.getOperationEndTime().toString());
+          setGraphic(layout);
+        }
+      }
+    });
+  }
+
+  public void tick() {
+    queuesListView.refresh();
+  }
+
+  private Machine getMachineFromQueueItem(QueueItem item) {
+    var machineTypes = App.getMachineTypes();
+    for (var machineType : machineTypes) {
+      var machines = machineType.getMachines();
+      for (Machine machine : machines) {
+        if (machine.getQueue().contains(item)) {
+          return machine;
+        }
+      }
+    }
+    return null;
+  }
+
+}
--- src/main/java/com/blastoisefx/model/User.java
+++ src/main/java/com/blastoisefx/model/User.java
@@ -2,14 +2,19 @@ package com.blastoisefx.model;
 
 import java.io.Serializable;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class User implements Serializable {
     private String email;
     private String password;
+    private ObservableList<QueueItem> queue;
 
     // create user by giving name and contact number
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+        queue = FXCollections.observableArrayList();
     }
 
     public String getEmail() {
@@ -18,5 +23,9 @@ public class User implements Serializable {
 
     public String getPassword() {
         return password;
+    }
+
+    public ObservableList<QueueItem> getQueue() {
+        return queue;
     }
 }
